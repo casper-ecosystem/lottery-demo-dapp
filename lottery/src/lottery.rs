8318d4b063e60d5ad17b9660e42f14aa32d82845
sczembor <stanislaw.czembor@gmail.com>
2024-03-26 13:09:57 +0100

    add rounds

--- lottery/src/lottery.rs
+++ lottery/src/lottery.rs
@@ -3,39 +3,60 @@ use odra::casper_types::U256;
 use odra::casper_types::U512;
 use odra::prelude::*;
 use odra::Address;
+use odra::Mapping;
 use odra::OdraError;
+use odra::OdraType;
 use odra::SubModule;
 use odra::Var;
 use odra_modules::access::Ownable;
 use odra_modules::erc721::erc721_base::Erc721Base;
 use odra_modules::erc721::Erc721;
 
+pub type RoundId = u32;
+
+#[derive(OdraType, PartialEq, Debug)]
+pub struct Round {
+    starts_at: u64,
+    ends_at: u64,
+    ticket_price: U512,
+    balance: U512,
+    winner: Option<Address>,
+    next_index: U256,
+}
+
+#[derive(OdraType, PartialEq, Debug)]
+pub struct RoundView {
+    starts_at: u64,
+    ends_at: u64,
+    ticket_price: U512,
+    prize_pool: U512,
+    winner: Option<Address>,
+    num_of_participants: U256,
+}
+
 #[derive(OdraError)]
 pub enum Error {
     LotteryIsNotActive = 1,
     WrongPayment = 2,
     LotteryInProgress = 3,
+    RoundNotFound = 4,
+    ContractPaused = 5,
 }
 
 #[odra::module]
 pub struct Lottery {
     ownable: SubModule<Ownable>,
     erc721: SubModule<Erc721Base>,
-    ticket_price: Var<U512>,
-    starts_at: Var<u64>,
-    ends_at: Var<u64>,
-    next_index: Var<U256>,
-    winner: Var<Address>,
+    rounds: Mapping<RoundId, Round>,
+    next_round: Var<u32>,
+    is_paused: Var<bool>,
 }
 
 #[odra::module]
 impl Lottery {
-    pub fn init(&mut self, ticket_price: U512, starts_at: u64, ends_at: u64) {
+    pub fn init(&mut self) {
         self.ownable.init();
-        self.ticket_price.set(ticket_price);
-        self.starts_at.set(starts_at);
-        self.ends_at.set(ends_at);
-        self.next_index.set(U256::zero());
+        self.next_round.set(1);
     }
 
     delegate! {
@@ -63,46 +84,134 @@ impl Lottery {
         }
     }
 
-    pub fn ticket_price(&self) -> U512 {
-        self.ticket_price.get_or_default()
+    // Admnin
+    pub fn pause(&mut self) {
+        self.ownable.assert_owner(&self.env().caller());
+        self.is_paused.set(true);
     }
 
+    pub fn unpause(&mut self) {
+        self.ownable.assert_owner(&self.env().caller());
+        self.is_paused.set(false);
+    }
+
+    pub fn create_round(&mut self, starts_at: u64, ends_at: u64, ticket_price: U512) -> RoundId {
+        self.assert_not_paused();
+        self.ownable.assert_owner(&self.env().caller());
+        let current_round = self.next_round.get_or_default();
+        self.rounds.set(
+            &current_round,
+            Round {
+                starts_at,
+                ends_at,
+                ticket_price,
+                balance: U512::zero(),
+                winner: None,
+                next_index: U256::zero(),
+            },
+        );
+        self.next_round.add(1);
+        current_round
+    }
     #[odra(payable)]
-    pub fn buy_ticket(&mut self) {
-        self.assert_active();
-        self.assert_deposit();
-        let caller = self.env().caller();
-        self.erc721.balances.add(&caller, U256::one());
-        self.erc721
-            .owners
-            .set(&self.next_index.get_or_default(), Some(caller));
-        self.next_index.add(U256::one());
-    }
-
-    pub fn starts_at(&self) -> u64 {
-        self.starts_at.get_or_default()
-    }
-
-    pub fn winner(&self) -> Address {
-        match self.winner.get() {
-            None => self.env().revert(Error::LotteryInProgress),
-            Some(addr) => return addr,
+    pub fn top_up_prize_pool(&mut self, round_id: RoundId) {
+        self.ownable.assert_owner(&self.env().caller());
+        match self.rounds.get(&round_id) {
+            Some(mut r) => {
+                r.balance += self.env().attached_value();
+                self.rounds.set(&round_id, r);
+            }
+            None => self.env().revert(Error::RoundNotFound),
         }
     }
 
-    pub fn resolve_lottery(&mut self, seed: [u8; 32]) {
+    pub fn resolve_lottery(&mut self, round_id: RoundId, seed: [u8; 32]) {
         self.ownable.assert_owner(&self.env().caller());
-        let winner_index = self.get_random_number(seed);
+        let winner_index = self.get_random_number(seed, round_id);
         let winner_addr = self.owner_of(&U256::from(winner_index));
         self.env()
             .transfer_tokens(&winner_addr, &self.env().self_balance());
-        self.winner.set(winner_addr);
+        match self.rounds.get(&round_id) {
+            Some(mut r) => {
+                r.winner = Some(winner_addr);
+                self.rounds.set(&round_id, r);
+            }
+            None => self.env().revert(Error::RoundNotFound),
+        }
     }
 
-    pub fn get_random_number(&self, seed: [u8; 32]) -> u64 {
-        let timestamp = self.env().get_block_time();
-        let max: u64 = self.next_index.get_or_default().as_u64();
+    // Transactions
+    #[odra(payable)]
+    pub fn play_lottery(&mut self, round_id: RoundId) {
+        self.assert_not_paused();
+        self.assert_active(round_id);
+        self.assert_deposit(round_id);
+        let caller = self.env().caller();
+        self.erc721.balances.add(&caller, U256::one());
+        match self.rounds.get(&round_id) {
+            Some(mut r) => {
+                let round_id_shifted = (round_id as u64) << 16;
+                self.erc721.owners.set(
+                    &U256::from(round_id_shifted | r.next_index.as_u64()),
+                    Some(caller),
+                );
+                r.next_index += U256::one();
+                self.rounds.set(&round_id, r);
+            }
+            None => self.env().revert(Error::RoundNotFound),
+        };
+    }
 
+    // Queries
+    pub fn starts_at(&self, round_id: RoundId) -> Option<u64> {
+        match self.rounds.get(&round_id) {
+            Some(r) => Some(r.starts_at),
+            None => None,
+        }
+    }
+
+    pub fn ends_at(&self, round_id: RoundId) -> Option<u64> {
+        match self.rounds.get(&round_id) {
+            Some(r) => Some(r.ends_at),
+            None => None,
+        }
+    }
+
+    pub fn ticket_price(&self, round_id: RoundId) -> Option<U512> {
+        match self.rounds.get(&round_id) {
+            Some(r) => Some(r.ticket_price),
+            None => None,
+        }
+    }
+
+    pub fn winner(&self, round_id: RoundId) -> Option<Address> {
+        match self.rounds.get(&round_id) {
+            Some(r) => r.winner,
+            None => None,
+        }
+    }
+
+    pub fn round_view(&self, round_id: RoundId) -> Option<RoundView> {
+        match self.rounds.get(&round_id) {
+            Some(r) => Some(RoundView {
+                starts_at: r.starts_at,
+                ends_at: r.ends_at,
+                ticket_price: r.ticket_price,
+                winner: r.winner,
+                prize_pool: r.balance,
+                num_of_participants: r.next_index,
+            }),
+            None => None,
+        }
+    }
+
+    // Internal
+    fn get_random_number(&self, seed: [u8; 32], round_id: RoundId) -> u64 {
+        let timestamp = self.env().get_block_time();
+        let max = match self.rounds.get(&round_id) {
+            Some(r) => r.next_index.as_u64(),
+            None => self.env().revert(Error::RoundNotFound),
+        };
         let mut salted_seed = [0u8; 80];
 
         salted_seed[..32].copy_from_slice(&seed);
@@ -111,21 +220,44 @@ impl Lottery {
 
         let hashed_data = self.env().hash(&salted_seed);
 
-        u64::from_be_bytes(hashed_data[..8].try_into().unwrap()) % max
+        let random_number = u64::from_be_bytes(hashed_data[..8].try_into().unwrap()) % max;
+
+        // Combine the round_id (converted to u64) and the random number into a single u64 value
+        // We use bit shifting to achieve the desired placement
+
+        // Shift the round_id 16 bits to the left to make space for the random number at the ones place
+        let round_id_shifted = (round_id as u64) << 16;
+
+        // Combine the shifted round_id and the random number using bitwise OR
+        round_id_shifted | random_number
     }
 
-    fn assert_active(&self) {
+    fn assert_active(&self, round_id: RoundId) {
         let current_timestamp = self.env().get_block_time();
-        if self.starts_at.get_or_default() > current_timestamp
-            || self.ends_at.get_or_default() < current_timestamp
-        {
-            self.env().revert(Error::LotteryIsNotActive);
+        match self.rounds.get(&round_id) {
+            Some(r) => {
+                if r.starts_at > current_timestamp || r.ends_at < current_timestamp {
+                    self.env().revert(Error::LotteryIsNotActive);
+                }
+            }
+            None => self.env().revert(Error::RoundNotFound),
         }
     }
 
-    fn assert_deposit(&self) {
-        if self.env().attached_value() != self.ticket_price.get_or_default() {
-            self.env().revert(Error::WrongPayment);
+    fn assert_not_paused(&self) {
+        if self.is_paused.get_or_default() {
+            self.env().revert(Error::ContractPaused)
+        }
+    }
+
+    fn assert_deposit(&self, round_id: RoundId) {
+        match self.rounds.get(&round_id) {
+            Some(r) => {
+                if self.env().attached_value() != r.ticket_price {
+                    self.env().revert(Error::WrongPayment);
+                }
+            }
+            None => self.env().revert(Error::RoundNotFound),
         }
     }
 }
