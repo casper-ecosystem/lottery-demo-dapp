10f983e402880999a644abd5b8bc232270655028
sczembor <stanislaw.czembor@gmail.com>
2024-03-26 21:51:10 +0100

    add consolation prizes; update unit test + events

--- smart-contract/lottery/src/lottery.rs
+++ smart-contract/lottery/src/lottery.rs
@@ -1,3 +1,5 @@
+use core::cmp::min;
+
 use odra::casper_types::bytesrepr::Bytes;
 use odra::casper_types::U256;
 use odra::casper_types::U512;
@@ -14,26 +16,16 @@ use odra_modules::erc721::erc721_base::Erc721Base;
 use odra_modules::erc721::Erc721;
 
 pub type RoundId = u32;
-const TICKETS_PER_ROUND: u64 = 10_000;
+const ONE_CSPR: u64 = 1_000_000_000;
+const SEED: [u8; 32] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+    27, 28, 29, 30, 31, 32,
+];
 
 #[derive(OdraType, PartialEq, Debug)]
 pub struct Round {
     starts_at: u64,
     ends_at: u64,
-    ticket_price: U512,
-    balance: U512,
-    winner: Option<Address>,
-    next_index: U256,
-}
-
-#[derive(OdraType, PartialEq, Debug)]
-pub struct RoundView {
-    starts_at: u64,
-    ends_at: u64,
-    ticket_price: U512,
-    prize_pool: U512,
-    winner: Option<Address>,
-    num_of_participants: U256,
 }
 
 #[derive(OdraError)]
@@ -46,36 +38,52 @@ pub enum Error {
 }
 
 #[derive(Event, PartialEq, Eq, Debug)]
-pub struct PlayEvent {
+pub struct Play {
     pub round_id: RoundId,
     pub player: Address,
-    pub ticket_id: U256,
+    pub play_id: U256,
     pub timestamp: u64,
-}
-
-#[derive(Event, PartialEq, Eq, Debug)]
-pub struct WinEvent {
-    pub round_id: RoundId,
-    pub winner: Address,
-    pub ticket_id: U256,
     pub prize: U512,
-    pub timestamp: u64,
+    pub is_jackpot: bool,
+}
+enum Outcome {
+    Jackpot,
+    ConsolationPrize(U512),
+    NoWin,
 }
 
-#[odra::module(events = [PlayEvent, WinEvent])]
+#[odra::module(events = [Play])]
 pub struct Lottery {
     ownable: SubModule<Ownable>,
     erc721: SubModule<Erc721Base>,
     rounds: Mapping<RoundId, Round>,
     next_round: Var<u32>,
+    next_play_id: Var<U256>,
     is_paused: Var<bool>,
+    collected_fees: Var<U512>,
+    prize_pool: Var<U512>,
+    max_consolation_prize: Var<U512>,
+    lottery_fee: Var<U512>,
+    jackpot_probability: Var<u8>,
+    consolation_prize_probability: Var<u8>,
+    active_round: Var<u32>,
+    ticket_price: Var<U512>,
 }
 
 #[odra::module]
 impl Lottery {
-    pub fn init(&mut self) {
+    pub fn init(&mut self, jackpot_probability: u8, consolation_prize_probability: u8) {
         self.ownable.init();
         self.next_round.set(1);
+        self.collected_fees.set(U512::zero());
+        self.prize_pool.set(U512::zero());
+        self.lottery_fee.set(U512::from(ONE_CSPR));
+        self.ticket_price.set(U512::from(50 * ONE_CSPR));
+        self.max_consolation_prize.set(U512::from(50 * ONE_CSPR));
+        self.jackpot_probability.set(jackpot_probability);
+        self.consolation_prize_probability
+            .set(consolation_prize_probability);
+        self.next_play_id.set(U256::one());
     }
 
     delegate! {
@@ -103,7 +111,7 @@ impl Lottery {
         }
     }
 
-    // Admnin
+    // Admin
     pub fn pause(&mut self) {
         self.ownable.assert_owner(&self.env().caller());
         self.is_paused.set(true);
@@ -114,157 +122,135 @@ impl Lottery {
         self.is_paused.set(false);
     }
 
-    pub fn create_round(&mut self, starts_at: u64, ends_at: u64, ticket_price: U512) -> RoundId {
+    pub fn create_round(&mut self, starts_at: u64, ends_at: u64) -> RoundId {
         self.assert_not_paused();
         self.ownable.assert_owner(&self.env().caller());
         let current_round = self.next_round.get_or_default();
-        self.rounds.set(
-            &current_round,
-            Round {
-                starts_at,
-                ends_at,
-                ticket_price,
-                balance: U512::zero(),
-                winner: None,
-                next_index: U256::zero(),
-            },
-        );
+        self.rounds
+            .set(&current_round, Round { starts_at, ends_at });
         self.next_round.add(1);
+        self.active_round.set(current_round);
         current_round
     }
     #[odra(payable)]
-    pub fn top_up_prize_pool(&mut self, round_id: RoundId) {
+    pub fn top_up_prize_pool(&mut self) {
         self.ownable.assert_owner(&self.env().caller());
-        match self.rounds.get(&round_id) {
-            Some(mut r) => {
-                r.balance += self.env().attached_value();
-                self.rounds.set(&round_id, r);
-            }
-            None => self.env().revert(Error::RoundNotFound),
-        }
-    }
-
-    pub fn resolve_lottery(&mut self, round_id: RoundId, seed: [u8; 32]) {
-        self.ownable.assert_owner(&self.env().caller());
-        match self.rounds.get(&round_id) {
-            Some(mut r) => {
-                let timestamp = self.env().get_block_time();
-                if timestamp <= r.ends_at {
-                    self.env().revert(Error::LotteryInProgress)
-                }
-                let ticket_id = U256::from(self.get_random_number(seed, round_id));
-                let winner = self.owner_of(&ticket_id);
-                self.env().transfer_tokens(&winner, &r.balance);
-                self.env().emit_event(WinEvent {
-                    round_id,
-                    winner,
-                    ticket_id,
-                    prize: r.balance,
-                    timestamp,
-                });
-                r.winner = Some(winner);
-                self.rounds.set(&round_id, r);
-            }
-            None => self.env().revert(Error::RoundNotFound),
-        }
+        self.prize_pool.add(self.env().attached_value());
     }
 
     // Transactions
     #[odra(payable)]
-    pub fn play_lottery(&mut self, round_id: RoundId) {
+    pub fn play_lottery(&mut self) {
         self.assert_not_paused();
-        self.assert_active(round_id);
-        self.assert_deposit(round_id);
+        self.assert_active();
+        self.assert_deposit();
+        let round_id = self.active_round.get_or_default();
+        let play_id = self.next_play_id.get_or_default();
         let caller = self.env().caller();
+        self.collected_fees.add(self.lottery_fee.get_or_default());
+        self.prize_pool
+            .add(self.env().attached_value() - self.lottery_fee.get_or_default());
         self.erc721.balances.add(&caller, U256::one());
-        match self.rounds.get(&round_id) {
-            Some(mut r) => {
-                let round_id_shifted = (round_id as u64) * TICKETS_PER_ROUND;
-                let ticket_id = U256::from(round_id_shifted + r.next_index.as_u64());
-                self.erc721.owners.set(&ticket_id, Some(caller));
-                r.next_index += U256::one();
-                r.balance += self.env().attached_value();
-                self.rounds.set(&round_id, r);
-                self.env().emit_event(PlayEvent {
+        self.erc721.owners.set(&play_id, Some(caller));
+        self.next_play_id.add(U256::one());
+        match self.determine_outcome() {
+            Outcome::Jackpot => {
+                let prize = self.prize_pool.get_or_default();
+                self.env().transfer_tokens(&caller, &prize);
+                self.prize_pool.set(U512::zero());
+                self.env().emit_event(Play {
                     round_id,
                     player: caller,
-                    ticket_id,
+                    play_id,
                     timestamp: self.env().get_block_time(),
+                    is_jackpot: true,
+                    prize,
                 })
             }
-            None => self.env().revert(Error::RoundNotFound),
-        };
+            Outcome::ConsolationPrize(prize) => {
+                self.env().transfer_tokens(&caller, &prize);
+                self.prize_pool.subtract(prize);
+                self.env().emit_event(Play {
+                    round_id,
+                    player: caller,
+                    play_id,
+                    timestamp: self.env().get_block_time(),
+                    is_jackpot: false,
+                    prize,
+                })
+            }
+            Outcome::NoWin => self.env().emit_event(Play {
+                round_id,
+                player: caller,
+                play_id,
+                timestamp: self.env().get_block_time(),
+                is_jackpot: false,
+                prize: U512::zero(),
+            }),
+        }
     }
 
     // Queries
-    pub fn starts_at(&self, round_id: RoundId) -> Option<u64> {
+    pub fn starts_at(&self, round_id: Option<RoundId>) -> Option<u64> {
+        let round_id = round_id.unwrap_or(self.active_round.get_or_default());
         match self.rounds.get(&round_id) {
             Some(r) => Some(r.starts_at),
             None => None,
         }
     }
 
-    pub fn ends_at(&self, round_id: RoundId) -> Option<u64> {
+    pub fn ends_at(&self, round_id: Option<RoundId>) -> Option<u64> {
+        let round_id = round_id.unwrap_or(self.active_round.get_or_default());
         match self.rounds.get(&round_id) {
             Some(r) => Some(r.ends_at),
             None => None,
         }
     }
 
-    pub fn ticket_price(&self, round_id: RoundId) -> Option<U512> {
-        match self.rounds.get(&round_id) {
-            Some(r) => Some(r.ticket_price),
-            None => None,
-        }
-    }
-
-    pub fn winner(&self, round_id: RoundId) -> Option<Address> {
-        match self.rounds.get(&round_id) {
-            Some(r) => r.winner,
-            None => None,
-        }
-    }
-
-    pub fn round_view(&self, round_id: RoundId) -> Option<RoundView> {
-        match self.rounds.get(&round_id) {
-            Some(r) => Some(RoundView {
-                starts_at: r.starts_at,
-                ends_at: r.ends_at,
-                ticket_price: r.ticket_price,
-                winner: r.winner,
-                prize_pool: r.balance,
-                num_of_participants: r.next_index,
-            }),
-            None => None,
-        }
+    pub fn ticket_price(&self) -> Option<U512> {
+        self.ticket_price.get()
     }
 
     // Internal
-    fn get_random_number(&self, seed: [u8; 32], round_id: RoundId) -> u64 {
+    fn determine_outcome(&self) -> Outcome {
+        let random_number = self.get_random_number();
+        let total_probability = self.jackpot_probability.get_or_default()
+            + self.consolation_prize_probability.get_or_default();
+
+        if random_number <= self.jackpot_probability.get_or_default().into() {
+            return Outcome::Jackpot;
+        } else if random_number <= total_probability.into() {
+            let max_consolation_prize = min(
+                self.max_consolation_prize.get_or_default(),
+                self.prize_pool.get_or_default(),
+            )
+            .as_u128();
+            let scaling_factor = max_consolation_prize / 100;
+            let consolation_prize =
+                U512::from((scaling_factor * random_number as u128) % max_consolation_prize);
+            return Outcome::ConsolationPrize(consolation_prize);
+        } else {
+            return Outcome::NoWin;
+        }
+    }
+
+    fn get_random_number(&self) -> u64 {
         let timestamp = self.env().get_block_time();
-        let max = match self.rounds.get(&round_id) {
-            Some(r) => r.next_index.as_u64(),
-            None => self.env().revert(Error::RoundNotFound),
-        };
         let mut salted_seed = [0u8; 80];
 
-        salted_seed[..32].copy_from_slice(&seed);
+        //TODO: replace with `random_bytes` function once its available
+        salted_seed[..32].copy_from_slice(&SEED);
 
         salted_seed[32..40].copy_from_slice(&timestamp.to_be_bytes());
 
         let hashed_data = self.env().hash(&salted_seed);
 
-        let random_number = u64::from_be_bytes(hashed_data[..8].try_into().unwrap()) % max;
-
-        let round_id_shifted = (round_id as u64) * TICKETS_PER_ROUND;
-
-        // Combine the shifted round_id and the random number
-        round_id_shifted + random_number
+        u64::from_be_bytes(hashed_data[..8].try_into().unwrap()) % 100
     }
 
-    fn assert_active(&self, round_id: RoundId) {
+    fn assert_active(&self) {
         let current_timestamp = self.env().get_block_time();
-        match self.rounds.get(&round_id) {
+        match self.rounds.get(&self.active_round.get_or_default()) {
             Some(r) => {
                 if r.starts_at > current_timestamp || r.ends_at < current_timestamp {
                     self.env().revert(Error::LotteryIsNotActive);
@@ -280,31 +266,22 @@ impl Lottery {
         }
     }
 
-    fn assert_deposit(&self, round_id: RoundId) {
-        match self.rounds.get(&round_id) {
-            Some(r) => {
-                if self.env().attached_value() != r.ticket_price {
-                    self.env().revert(Error::WrongPayment);
-                }
-            }
-            None => self.env().revert(Error::RoundNotFound),
+    fn assert_deposit(&self) {
+        if self.env().attached_value() != self.ticket_price.get_or_default() {
+            self.env().revert(Error::WrongPayment);
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::lottery::{Error, LotteryHostRef};
+    use crate::lottery::{Error, LotteryHostRef, LotteryInitArgs, Play, ONE_CSPR};
     use odra::{
         casper_types::{U256, U512},
-        host::{Deployer, HostRef, NoArgs},
+        host::{Deployer, HostRef},
     };
 
     const ONE_HOUR_IN_MILISECONDS: u64 = 3_600_000;
-    const SEED: [u8; 32] = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-        26, 27, 28, 29, 30, 31, 32,
-    ];
 
     #[test]
     fn flow1() {
@@ -315,20 +292,23 @@ mod tests {
         let charlie = env.get_account(3);
 
         env.set_caller(admin);
-        let mut contract = LotteryHostRef::deploy(&env, NoArgs);
-        let round_id = contract.create_round(
-            ONE_HOUR_IN_MILISECONDS,
-            3 * ONE_HOUR_IN_MILISECONDS,
-            U512::from(5),
+        let mut contract = LotteryHostRef::deploy(
+            &env,
+            LotteryInitArgs {
+                jackpot_probability: 30,
+                consolation_prize_probability: 50,
+            },
         );
-        assert_eq!(contract.ticket_price(round_id), Some(U512::from(5)));
-        assert_eq!(contract.starts_at(round_id), Some(ONE_HOUR_IN_MILISECONDS));
+        let round_id = contract.create_round(ONE_HOUR_IN_MILISECONDS, 3 * ONE_HOUR_IN_MILISECONDS);
+        assert_eq!(round_id, 1);
+        assert_eq!(contract.ticket_price(), Some(U512::from(50 * ONE_CSPR)));
+        assert_eq!(contract.starts_at(None), Some(ONE_HOUR_IN_MILISECONDS));
 
         env.set_caller(alice);
         assert_eq!(
             contract
-                .with_tokens(U512::from(5))
-                .try_play_lottery(round_id),
+                .with_tokens(U512::from(50 * ONE_CSPR))
+                .try_play_lottery(),
             Err(Error::LotteryIsNotActive.into())
         );
 
@@ -336,37 +316,72 @@ mod tests {
 
         assert_eq!(
             contract
-                .with_tokens(U512::from(1))
-                .try_play_lottery(round_id),
+                .with_tokens(U512::from(1 * ONE_CSPR))
+                .try_play_lottery(),
             Err(Error::WrongPayment.into())
         );
 
-        contract.with_tokens(U512::from(5)).play_lottery(round_id);
-
+        contract
+            .with_tokens(U512::from(50 * ONE_CSPR))
+            .play_lottery();
+        assert!(env.emitted_event(
+            contract.address(),
+            &Play {
+                round_id: 1,
+                player: alice,
+                play_id: U256::from(1),
+                prize: U512::from(15190000000 as u128),
+                is_jackpot: false,
+                timestamp: ONE_HOUR_IN_MILISECONDS,
+            },
+        ));
+        assert_eq!(env.events_count(contract.address()), 2);
         assert_eq!(contract.balance_of(&alice), U256::one());
-        assert_eq!(contract.owner_of(&U256::from(10000)), alice);
+        assert_eq!(contract.owner_of(&U256::from(1)), alice);
 
         env.set_caller(bob);
-        contract.with_tokens(U512::from(5)).play_lottery(round_id);
+        contract
+            .with_tokens(U512::from(50 * ONE_CSPR))
+            .play_lottery();
 
+        assert!(env.emitted_event(
+            contract.address(),
+            &Play {
+                round_id: 1,
+                player: bob,
+                play_id: U256::from(2),
+                prize: U512::from(15500000000 as u128),
+                is_jackpot: false,
+                timestamp: ONE_HOUR_IN_MILISECONDS,
+            },
+        ));
+        assert_eq!(env.events_count(contract.address()), 3);
         assert_eq!(contract.balance_of(&bob), U256::one());
-        assert_eq!(contract.owner_of(&U256::from(10001)), bob);
+        assert_eq!(contract.owner_of(&U256::from(2)), bob);
 
+        let inital_balance = env.balance_of(&charlie);
         env.set_caller(charlie);
-        contract.with_tokens(U512::from(5)).play_lottery(round_id);
+        contract
+            .with_tokens(U512::from(50 * ONE_CSPR))
+            .play_lottery();
 
+        assert!(env.emitted_event(
+            contract.address(),
+            &Play {
+                round_id: 1,
+                player: charlie,
+                play_id: U256::from(3),
+                prize: U512::from(15500000000 as u128),
+                is_jackpot: false,
+                timestamp: ONE_HOUR_IN_MILISECONDS,
+            },
+        ));
+        assert_eq!(env.events_count(contract.address()), 4);
         assert_eq!(contract.balance_of(&charlie), U256::one());
-        assert_eq!(contract.owner_of(&U256::from(10002)), charlie);
-
-        let bob_inital_balance = env.balance_of(&bob);
-        env.set_caller(admin);
+        assert_eq!(contract.owner_of(&U256::from(3)), charlie);
         assert_eq!(
-            contract.try_resolve_lottery(round_id, SEED),
-            Err(Error::LotteryInProgress.into())
-        );
-        env.advance_block_time(3 * ONE_HOUR_IN_MILISECONDS);
-        contract.resolve_lottery(round_id, SEED);
-        assert_eq!(contract.winner(round_id), Some(bob));
-        assert_eq!(bob_inital_balance + 15, env.balance_of(&bob))
+            inital_balance - U512::from(50 * ONE_CSPR) + U512::from(15500000000 as u128),
+            env.balance_of(&charlie)
+        )
     }
 }
