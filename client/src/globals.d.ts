4a331a531b758f6276d4f7fd60d0d6e5692ec4d7
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2024-09-05 11:56:31 +0200

    Removed unused configuration in the Web Client

--- client/src/globals.d.ts
+++ client/src/globals.d.ts
@@ -1,6 +1,5 @@
 type GlobalConfig = {
 	lottery_app_contract_package_hash: string;
-	lottery_play_payment_amount: string;
 	lottery_api_url: string;
 	lottery_api_ws_url: string;
 	cspr_click_app_name: string;
