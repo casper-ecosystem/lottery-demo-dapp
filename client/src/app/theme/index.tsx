4a5ce71187206ba6d1a1e37b1dd248541e4c0c2b
sasha kushnir <sasha@make.sevices>
2025-09-15 11:34:34 +0300

    changed theme import

--- client/src/app/theme/index.tsx
+++ client/src/app/theme/index.tsx
@@ -1,22 +1,7 @@
-import { CsprClickThemes } from '@make-software/csprclick-ui';
+import { themeConfig } from '@make-software/cspr-design';
 export const AppTheme = {
-	dark: {
-		...CsprClickThemes.dark,
-		topBarBackground: 'rgb(19, 25, 51)',
-		backgroundColor: '#0f1429',
-		clickLogo: '#ff0012',
-		contentPrimary: '#DADCE5',
-		contentSecondary: '#A8ADBF',
-		fillSecondary: '#1B254A',
-		backgroundPrimary: '#161D3B',
-		sectionBackground: '#050B23',
-		fillPrimaryBlue: '#8FA6FF',
-		linkColor: 'rgb(218, 220, 229)',
-		containerBackground: '#2d2d2d',
-		codeColor: '#D9D9D9',
-	},
 	light: {
-		...CsprClickThemes.light,
+		...themeConfig.light,
 		topBarBackground: 'rgb(24, 29, 64)',
 		backgroundColor: '#f2f3f5',
 		clickLogo: '#ff0012',
